726f75c38bdd45397526b63b6925e7945ea23a10
Denis Klochov <den_klochkov@mail.ru>
2023-02-16 01:15:23 +0300

    first cmmit

---
+++ home_work_1.py
@@ -0,0 +1,166 @@
+class Student:
+    def __init__(self, name, surname, gender):
+        self.name = name
+        self.surname = surname
+        self.gender = gender
+        self.finished_courses = []
+        self.courses_in_progress = []
+        self.grades = {}
+    def rate_hw(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in lecturer.courses_attached and course in self.courses_in_progress:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+    def middle(self):
+        self.grade_student = []
+        for v in self.grades.values():
+            self.grade_student.extend(v)
+        middle_grade = round(sum(self.grade_student) / len(self.grade_student), 2)  
+        return middle_grade
+    def __str__(self):
+        print('Имя студента:', self.name,'\nФамилия студента:', self.surname, '\nСредняя оценка за домашние задания:', self.middle(), '\nКурсы в процессе изучения:', self.courses_in_progress, '\nЗавершенные курсы:', self.finished_courses)  
+        return 
+    def __lt__(self, other):
+        if self.middle() > other.middle():
+            print('Лучший студент -', self.name, self.surname)
+        elif self.middle() == other.middle():
+             print('Оба хороши!')
+        else: 
+            print('Лучший студент -', other.name, other.surname)
+        return
+    
+class Mentor:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+        self.courses_attached = []
+        
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.grades = {}
+    def middle(self):
+        self.grade_lecture = []
+        for v in self.grades.values():
+            self.grade_lecture.extend(v)
+        middle_grade = round(sum(self.grade_lecture) / len(self.grade_lecture), 2)
+        return middle_grade
+    def __str__(self):
+        res = print('Имя лектора:', self.name, '\nФамилия лектора:', self.surname, '\nСредняя оценка за лекции:', self.middle())
+        return res
+    def __lt__(self, other):
+        if self.middle() > other.middle():
+            print('Лучший лектор -', self.name, self.surname)
+        elif self.middle() == other.middle():
+            print('Оба хороши!')
+        else:
+            print('Лучший лектор -', other.name, other.surname)        
+        return  
+class Reviewer(Mentor):
+    def rate_hw(self, student, course, grade):
+        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+            if course in student.grades:
+                student.grades[course] += [grade]
+            else:
+                student.grades[course] = [grade]
+        else:
+            return 'Ошибка'  
+    def __str__(self):
+        print('Имя эксперта:', self.name, '\nФамилия эксперта:', self.surname)
+        return 
+
+first_student = Student('Петр', 'Гавриков', 'мужчина')
+second_student = Student('Татьяна','Фролова', 'женщина')
+first_student.courses_in_progress += ['Python', 'Git']
+second_student.courses_in_progress += ['Python', 'Git']
+first_student.finished_courses += ['Pascal']
+second_student.finished_courses += ['Math']
+
+first_mentor = Mentor('Федор','Прокопьев')
+second_mentor = Mentor('Тамара', 'Щаулина')
+first_mentor.courses_attached += ['Python', 'Git']
+second_mentor.courses_attached += ['Python', 'Git']
+
+first_lecturer = Lecturer('Антон', 'Ратников')
+second_lecturer = Lecturer('Максим', 'Мушкетов')
+first_lecturer.courses_attached += ['Python', 'Git']
+second_lecturer.courses_attached += ['Python', 'Git']
+
+first_reviewer = Reviewer('Владимир', 'Медведев')
+second_reviewer = Reviewer('Алексей', 'Астахов')
+first_reviewer.courses_attached += ['Python', 'Git']
+second_reviewer.courses_attached += ['Python', 'Git']
+
+first_student.rate_hw(first_lecturer, 'Python', 91)
+first_student.rate_hw(first_lecturer, 'Python', 93)
+first_student.rate_hw(first_lecturer, 'Python', 77)
+first_student.rate_hw(first_lecturer, 'Git', 71)
+first_student.rate_hw(first_lecturer, 'Git', 80)
+first_student.rate_hw(first_lecturer, 'Git', 69)
+second_student.rate_hw(second_lecturer, 'Python', 81)
+second_student.rate_hw(second_lecturer, 'Python', 98)
+second_student.rate_hw(second_lecturer, 'Python', 84)
+second_student.rate_hw(second_lecturer, 'Git', 91)
+second_student.rate_hw(second_lecturer, 'Git', 88)
+second_student.rate_hw(second_lecturer, 'Git', 84)
+
+first_reviewer.rate_hw(first_student, 'Python', 89)
+first_reviewer.rate_hw(first_student, 'Python', 93)
+first_reviewer.rate_hw(first_student, 'Python', 99)
+first_reviewer.rate_hw(first_student, 'Git', 80)
+first_reviewer.rate_hw(first_student, 'Git', 83)
+first_reviewer.rate_hw(first_student, 'Git', 89)
+second_reviewer.rate_hw(second_student, 'Python', 85)
+second_reviewer.rate_hw(second_student, 'Python', 72)
+second_reviewer.rate_hw(second_student, 'Python', 78)
+second_reviewer.rate_hw(second_student, 'Git', 81)
+second_reviewer.rate_hw(second_student, 'Git', 60)
+second_reviewer.rate_hw(second_student, 'Git', 74)
+
+first_student.__str__()
+second_student.__str__()
+first_student.__lt__(second_student)
+
+first_lecturer.__str__()
+second_lecturer.__str__()
+first_lecturer.__lt__(second_lecturer)
+
+first_reviewer.__str__()
+second_reviewer.__str__()
+
+def middle_grade_student(first_student, second_student, course):
+  overal_grades = 0
+  count_grades = 0
+  if course in first_student.grades.keys():
+    for grades in first_student.grades[course]:
+      overal_grades += grades
+    count_grades += len(first_student.grades[course])
+    if course in second_student.grades.keys():    
+      for grades in second_student.grades[course]:
+        overal_grades += grades
+      count_grades += len(second_student.grades[course])
+  print('Средняя оценка по курсу', course, 'у студентов составляет:', round(overal_grades / count_grades, 2))
+  return   
+  
+middle_grade_student(first_student, second_student, 'Python') 
+middle_grade_student(first_student, second_student, 'Git') 
+
+def middle_grade_lecturer(first_lecturer, second_lecturer, course):
+  overal_grades = 0
+  count_grades = 0
+  if course in first_lecturer.grades.keys():
+    for grades in first_lecturer.grades[course]:
+      overal_grades += grades
+    count_grades += len(first_lecturer.grades[course])
+    if course in second_lecturer.grades.keys():    
+      for grades in second_lecturer.grades[course]:
+        overal_grades += grades
+      count_grades += len(second_lecturer.grades[course])
+  print('Средняя оценка по курсу', course, 'у лекторов составляет:', round(overal_grades / count_grades, 2))
+     
+  
+middle_grade_lecturer(first_lecturer, second_lecturer, 'Python')
+middle_grade_lecturer(first_lecturer, second_lecturer, 'Git') 
